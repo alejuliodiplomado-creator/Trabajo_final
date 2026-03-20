5f1f6e5353ec77ff0b2445b4f88bb320dd8372d6
Alejandra Julio <ale.julio.diplomado@gmail.com>
2026-03-20 11:47:34 -0300

    Primera aproximacion de lo que espero desarrollar en este trabajo final

--- Trabajo_final.R
+++ Trabajo_final.R
@@ -1,5 +1,21 @@
-print ("primera prueba de github")
-library(readxl)
-library(dplyr)
-library(lubridate)
-library(ggplot2)
+# ============================================================
+# Trabajo final diplomado.
+#Alejandra Julio R.
+
+#El siguiente scrip contiene todo lo necesario para resolver los siguientes desafios y/o
+#necesidades que tiene esta empresa fintech en el corto plazo. Estos son puntos que se han
+#desarrollado a lo largo del diplomado y que ahora seran complidos y mejorados con la 
+#retroalimentacion de los usuarios finales.
+
+# Desafios:
+
+# 1. Donstruir una base analitica de cobranzas, que permita identificar el estado de 
+#las facturas, calcular dias de mora y gneerar un resumen de riesgo por cliente, a partir 
+#de datos operaciones de facturacion y clientes
+
+# 2. Desarrollar un mondelo de redes neuronales (ANN) el cual sera capaz de clasificar 
+#empresas segun su nivel de riesgo de credito (evalucion de clientes).
+
+#Ambos puntos estan orientados a las areas comercial y financiera para la toma de decisiones.
+
+# ============================================================
